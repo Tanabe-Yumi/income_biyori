7bd05bcfd1f224719dad602c9e271f5199006b5a
Tanabe-Yumi <alleyoop58dank@gmail.com>
2025-10-05 11:43:38 +0900

    Show basic table

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 
 import BasicTable from './component/BasicTable'
+import DataTable from './component/DataTable'
 
 function App() {
   return (
     <>
-      <BasicTable />
+      {/* <BasicTable /> */}
+      <DataTable />
     </>
   )
 }
