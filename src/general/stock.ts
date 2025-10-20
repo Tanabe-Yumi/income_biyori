b16148388e9a6d32fe3aa1e656e08a59277fae84
Tanabe-Yumi <alleyoop58dank@gmail.com>
2025-10-20 22:01:06 +0900

    Cosolidated data classed.

--- src/general/stock.ts
+++ src/general/stock.ts
@@ -1,19 +1,24 @@
-export default class Stock {
-  // PK
+export default class StockData {
   private code: string;
   private name: string;
-  // FK
-  private market: number;
-  // FK
-  private sector: number;
+  private market: string;
+  private sector: string;
+  private price: number;
+  private dividend: number;
+  private yield: number;
+  private total_score: number;
   private created_at: Date;
   private updated_at: Date;
 
-  constructor(code: string, name: string, market: number, sector: number, created_at: Date, updated_at: Date) {
+  constructor(code: string, name: string, market: string, sector: string, price: number, dividend: number, dividend_yield: number, total_score: number, created_at: Date, updated_at: Date) {
     this.code = code;
     this.name = name;
     this.market = market;
     this.sector = sector;
+    this.price = price
+    this.dividend = dividend;
+    this.yield = dividend_yield;
+    this.total_score = total_score
     this.created_at = created_at;
     this.updated_at = updated_at;
   }
@@ -26,12 +31,28 @@ export default class Stock {
     return this.name;
   }
 
-  public getMarket(): number {
+  public getMarket(): string {
     return this.market;
   }
 
-  public getSector(): number {
+  public getSector(): string {
     return this.sector;
+  }
+
+  public getPrice(): number {
+    return this.price;
+  }
+
+  public getDividend(): number {
+    return this.dividend;
+  }
+
+  public getYield(): number {
+    return this.yield;
+  }
+
+  public getTotalScore(): number {
+    return this.total_score;
   }
 
   public getCreatedAt(): Date {
